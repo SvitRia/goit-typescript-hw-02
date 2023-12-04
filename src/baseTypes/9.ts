37b574ebf73e22ec55de2bdb34abcdee1c597a25
SvitRia <129522432+SvitRia@users.noreply.github.com>
2023-12-04 15:42:37 +0100

    update

--- src/baseTypes/9.ts
+++ src/baseTypes/9.ts
@@ -5,15 +5,17 @@ type Page = {
   title: string;
   likes: number;
   accounts: string[];
-  status: string;
-  details?:{}
+  status: "open" | "close";
+  details?: {
+    createAt: Date;
+    updateAt: Date;}
 }
 
 const page1:Page = {
   title: 'The awesome page',
   likes: 100,
   accounts: ['Max', 'Anton', 'Nikita'],
-  status: 'open',
+  status: 'open' ,
   details: {
     createAt: new Date('2021-01-01'),
     updateAt: new Date('2021-05-01'),
--- src/generic/3.ts
+++ src/generic/3.ts
@@ -8,4 +8,6 @@ function merge <T extends object, U extends object>(objA:T, objB:U) {
   return Object.assign(objA, objB);
 }
 
-export {}
+export { }
+
+
